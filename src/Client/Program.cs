92bea3b90dd4e60409a453b356231cc5e141e83d
mgx0612 <marcusgx0612@gmail.com>
2020-05-01 15:48:48 +0800

    identityserver4 quickstart

---
+++ src/Client/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main()
+        {
+
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+
+            }
+
+            var request = new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "mysecret",
+                Scope = "api12",
+            };
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(request);
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("Hello World!");
+
+            CallApi(tokenResponse.AccessToken);
+        }
+
+
+        static async void CallApi(string token)
+        {
+            var client = new HttpClient();
+            client.SetBearerToken(token);
+
+            var res = await client.GetAsync("http://localhost:5001/identity");
+            if (!res.IsSuccessStatusCode)
+            {
+                Console.WriteLine(res.StatusCode);
+            }
+            else
+            {
+                var content = await res.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+
+}
---
+++ src/Id4Server/Config.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Id4Server
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[]
+            {
+                new ApiResource("api12","My API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client{
+                    ClientId = "client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets = {
+                        new Secret("mysecret".Sha256())
+                    },
+                    AllowedScopes = {"api12"}
+                },
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientSecrets = {new Secret("mxsecret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireConsent = false,
+                    RequirePkce = true,
+                    RedirectUris = { "http://localhost:5002/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                    }
+                }
+             };
+
+    }
+}
